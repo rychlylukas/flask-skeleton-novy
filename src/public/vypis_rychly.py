5b2fe26192e441a164f1ecf71742a14917fd7a10
rychlylukas <46920773+rychlylukas@users.noreply.github.com>
2020-09-16 19:36:53 +0200

    Add files via upload

---
+++ src/public/vypis_rychly.py
@@ -0,0 +1,7 @@
+from flask import Blueprint,render_template
+from .views import blueprint
+@blueprint.route('/vypis_rychly', methods=['GET'])
+def vypis_rychly():
+        pole=[[0,1],[2,3],[0,30]]
+        pole[0][1]=pole[0][1]+1
+        return render_template("public/vypis_rychly.tmpl",data =pole)
