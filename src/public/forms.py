0970bbff4acc52f66a4a0c64b9cd8ceb1e025010
rychlylukas <46920773+rychlylukas@users.noreply.github.com>
2020-09-23 23:08:42 +0200

    Update forms.py

--- src/public/forms.py
+++ src/public/forms.py
@@ -1,8 +1,8 @@
 import re
 
 from flask_wtf import Form
-from wtforms.fields import BooleanField, TextField, PasswordField, DateTimeField, IntegerField,SelectField
-from wtforms.validators import EqualTo, Email, InputRequired, Length
+from wtforms.fields import BooleanField, TextField, PasswordField, DateTimeField, IntegerField,SelectField, FloatField
+from wtforms.validators import EqualTo, Email, InputRequired, Length, NumberRange
 
 from ..data.models import User, LogUser
 from ..fields import Predicate
@@ -44,3 +44,9 @@ class secti(Form):
 class masoform(Form):
     typ=SelectField('Typ', choices=[(1, "Hovezi"), (2, "Veprove")], default=2)
 
+class ocform(Form):
+    a = FloatField("Strana a:", validators=[InputRequired(message="vyzadovano")])
+    b = FloatField("Strana b:", validators=[InputRequired(message="vyzadovano")])
+    obrazec = SelectField("Obrazec", choices=[(1, "Ctverec a"),\
+                                              (2, "Obdelnik ab"),\
+                                              (3, "Trojuhelnik abc")], default=1)
